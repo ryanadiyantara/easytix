773cc44fd1028f230ada1ce88a8fa2477887d7ba
Ryan Adiyantara <adiyantararyan@gmail.com>
2025-01-27 11:38:41 +0700

    Add Navbar and Separator components; update Admin Dashboard and Change Password pages with layout and footer

--- frontend/src/pages/admin.dashboard.jsx
+++ frontend/src/pages/admin.dashboard.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+// import CustomModal from "../components/Modal";
+import Footer from "../components/Footer";
 
 const AdminDashboard = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <Sidebar />
+      <Box
+        w={{
+          base: "100%",
+          xl: "calc(100% - 275px)",
+        }}
+        float="right"
+        maxWidth="100%"
+        overflow="auto"
+        position="relative"
+        maxHeight="100%"
+        transition="all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)"
+        transitionDuration=".2s, .2s, .35s"
+        transitionProperty="top, bottom, width"
+        transitionTimingFunction="linear, linear, ease"
+      >
+        <Navbar />
+
+        <Footer />
+      </Box>
+    </>
   );
 };
 
